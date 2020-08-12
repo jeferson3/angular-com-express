70cd9d83e4c97ce97939400ea9ca9e6f1692afdd
jeferson <jefersongomes333@hotmail.com>
2020-08-12 10:41:04 -0300

    Update routes.js

--- api-express/src/routes.js
+++ api-express/src/routes.js
@@ -6,7 +6,7 @@ router.post('/login', controller.login)
 router.get('/users', userMiddleware, controller.get)
 router.get('/users/me', userMiddleware, controller.me)
 router.post('/users/new', controller.post)
-router.delete('users/:id', controller.delete)
-router.put('users/:id', controller.put)
+router.delete('/users/:id', controller.delete)
+router.put('/users/:id', controller.put)
 
 module.exports = router;
